9ab93777789c08d553d799a6cc342db4095e47e5
Fran0616 <59398607+Fran0616@users.noreply.github.com>
2020-08-18 20:22:51 -0400

    Add files via upload

---
+++ rightTriangle .py
@@ -0,0 +1,5 @@
+#this code will take user input from a right triangle and solve the area of the triangle
+print ("This code will find the hypotenus of a right triangle")
+legA = float(input("Input first leg length: "))
+legB = float(input("Input second leg length: "))
+print ("Hypotenuse leength is " + str((legA**2 + legB**2) ** .5))
